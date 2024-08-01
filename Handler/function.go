7ca7edf42be82eb2f9951e63c7f6408e7b4717f0
bachlang364 <bachlang364@gmail.com>
2024-08-01 19:43:51 +0700

    add verbs and function check words

--- Handler/function.go
+++ Handler/function.go
@@ -1,7 +1,60 @@
 package main
 
 import (
-
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
+func open_file(path string) (*os.File, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("lỗi khi mở file %s: %v", path, err)
+	}
+	return file, nil
+}
 
+func read_text(file *os.File) map[string]bool {
+	text_words := make(map[string]bool)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text_words[strings.ToLower(strings.TrimSpace(scanner.Text()))] = true
+	}
+	return text_words
+}
+
+func create_lack(path string) (*os.File, error) {
+	file, err := os.Create(path)
+	if err != nil {
+		return nil, fmt.Errorf("lỗi khi tạo file %s: %v", path, err)
+	}
+	return file, nil
+}
+
+func process_verb_line(line string, text_words map[string]bool, lack_word_file *os.File, skip_words map[string]bool) {
+	verbs := strings.Split(strings.TrimSuffix(line, ":"), "/")
+	for _, verb := range verbs {
+		verb = strings.ToLower(strings.TrimSpace(verb))
+		verb = strings.ReplaceAll(verb, "(uk)", "")
+		verb = strings.ReplaceAll(verb, "(us)", "")
+		if !skip_words[verb] && !text_words[verb] {
+			fmt.Fprintln(lack_word_file, verb)
+		}
+	}
+}
+
+func scan_verbs(verbs_file *os.File, text_words map[string]bool, lack_word_file *os.File) {
+	verb_scanner := bufio.NewScanner(verbs_file)
+	var skip_words = map[string]bool{
+		"ngữ cảnh": true,
+		"ví dụ":    true,
+	}
+
+	for verb_scanner.Scan() {
+		line := verb_scanner.Text()
+		if strings.HasSuffix(line, ":") {
+			process_verb_line(line, text_words, lack_word_file, skip_words)
+		}
+	}
+}
--- Handler/main.go
+++ Handler/main.go
@@ -1,7 +1,37 @@
 package main
 
-import (
-
-)
+import "fmt"
 
 
+func scan_verbs_and_compare() {
+	verbs_file, err := open_file("./Database/Data-txt/Verbs/verbs.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer verbs_file.Close()
+
+	text_file, err := open_file("./Database//Data-txt/Text.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer text_file.Close()
+
+	text_words := read_text(text_file)
+
+	lack_word_file, err := create_lack("lack-word.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer lack_word_file.Close()
+
+	scan_verbs(verbs_file, text_words, lack_word_file)
+
+	fmt.Println("Quá trình so sánh và ghi file hoàn tất.")
+}
+
+func main () {
+	scan_verbs_and_compare()
+}
