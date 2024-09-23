bacc34bee5a59f449e0cde1b8c8e1798997479d8
langbach364 <bachlang364@gmail.com>
2024-09-23 12:32:06 +0700

    add interface to search words

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,18 +2,40 @@ const app = Vue.createApp({
   data() {
     return {
       sentenceInfo: {},
-      wordsInfo: {}
+      wordsInfo: {},
+      newWord: ''
+    }
+  },
+  methods: {
+    sendWordToServer() {
+      fetch('http://localhost:7089/word', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ data: this.newWord }),
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+        this.newWord = '';
+        this.fetchDefinitions();
+      })
+      .catch((error) => console.error('Error:', error));
+    },
+    fetchDefinitions() {
+      fetch('../Handler/sourcegraph-cody/answer.txt')
+        .then(response => response.text())
+        .then(data => {
+          const parsedData = window.parseDefinitions(data);
+          this.sentenceInfo = parsedData.sentence;
+          this.wordsInfo = parsedData.words;
+        })
+        .catch(error => console.error('Error:', error));
     }
   },
   mounted() {
-    fetch('../Handler/sourcegraph-cody/answer.txt')
-      .then(response => response.text())
-      .then(data => {
-        const parsedData = window.parseDefinitions(data);
-        this.sentenceInfo = parsedData.sentence;
-        this.wordsInfo = parsedData.words;
-      })
-      .catch(error => console.error('Error:', error));
+    this.fetchDefinitions();
   }
 });
 
